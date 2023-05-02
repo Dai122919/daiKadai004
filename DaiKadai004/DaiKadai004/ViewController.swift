e6e0c7f361ea603d9068c2905f67bf75e99074c2
akio0911 <akio0911@gmail.com>
2023-05-02 18:23:40 +0900

    feedback

--- DaiKadai004/DaiKadai004/ViewController.swift
+++ DaiKadai004/DaiKadai004/ViewController.swift
@@ -10,19 +10,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var resultLabel: UILabel!
-    var number: Int! = 0
+    var number: Int = 0
 
     @IBAction func buttonPlus(_ sender: Any) {
-
         number += 1
         resultLabel.text = String(number)
-
     }
 
     @IBAction func clearButton(_ sender: Any) {
-
         number = 0
         resultLabel.text = String(number)
-
     }
 }
